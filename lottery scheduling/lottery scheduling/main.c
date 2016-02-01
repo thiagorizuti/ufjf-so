69a7f8cac2f5ee0e40ac2be09857b207c3327624
Thiago <t.rizuti@gmail.com>
2016-02-01 14:30:08 -0200

    Makefile

---
+++ lottery scheduling/lottery scheduling/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lottery scheduling
+//
+//  Created by Thiago Costa Rizuti da Rocha on 1/23/16.
+//  Copyright © 2016 thiago. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
